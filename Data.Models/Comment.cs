6ff5bace1c6f4ffc6bf27dfd65578c1e90effdfe
JayeFive <john.olson3@gmail.com>
2025-07-13 12:51:46 -0600

    Add project files.

---
+++ Data.Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models;
+
+public class Comment
+{
+    public string? Id { get; set; }
+    public string BlogPostId { get; set; }
+    public DateTime Date { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
